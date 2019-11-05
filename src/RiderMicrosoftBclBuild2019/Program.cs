2310fa2c043f9f6475fa701c3c713c22dc53f585
Thomas Harold <tgharold@gmail.com>
2019-11-05 06:39:28 -0500

    Create trivial demo solution

---
+++ src/RiderMicrosoftBclBuild2019/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+namespace RiderMicrosoftBclBuild2019
+{
+    public class Program
+    {
+        public static string GetBaseAddress(HttpClient client)
+        {
+            return client?.BaseAddress?.ToString();
+        }
+        
+        public static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine(GetBaseAddress(client));
+        }
+    }
+}
---
+++ tests/RiderMicrosoftBclBuild2019.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using Xunit;
+
+namespace RiderMicrosoftBclBuild2019.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var client = new HttpClient();
+            var result = Program.GetBaseAddress(client);
+            Assert.Equal(null, result);
+        }
+    }
+}
