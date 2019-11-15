8c00bff76b5c9e9dd17561dd0fe1a294f48a21f6
Thomas Harold <tgharold@gmail.com>
2019-11-15 08:09:16 -0500

    add "Microsoft.Net.Http" version="2.2.29"

--- src/RiderMicrosoftBclBuild2019/Class1.cs
+++ src/RiderMicrosoftBclBuild2019/Class1.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Http;
 
 namespace RiderMicrosoftBclBuild2019
 {
     public class Class1
     {
+        public HttpClient Method1()
+        {
+            var httpClient = new HttpClient();
+            return httpClient;
+        }
     }
 }
