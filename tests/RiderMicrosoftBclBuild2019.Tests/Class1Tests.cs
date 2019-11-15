1e859eaafec8fbdd73cc5d62dee2692db241adb4
Thomas Harold <tgharold@gmail.com>
2019-11-15 08:32:43 -0500

    Add test proj with EnsureNuGetPackageBuildImports

---
+++ tests/RiderMicrosoftBclBuild2019.Tests/Class1Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace RiderMicrosoftBclBuild2019.Tests
+{
+    public class Class1Tests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var sut = new Class1();
+            var result = sut.Method1();
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ tests/RiderMicrosoftBclBuild2019.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RiderMicrosoftBclBuild2019.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RiderMicrosoftBclBuild2019.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3dd7970-c17e-4f3a-b380-447721d5fa54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
